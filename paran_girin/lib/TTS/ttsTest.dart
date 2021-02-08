4cdb301f91a2e8e8814c1d6e0eae38b9210e1e30
unknown <ghqls1237@kaist.ac.kr>
2021-02-08 23:13:20 +0900

    audio start

--- paran_girin/lib/TTS/ttsTest.dart
+++ paran_girin/lib/TTS/ttsTest.dart
@@ -3,18 +3,14 @@ import 'package:paran_girin/theme/colors.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'dart:convert';
+import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(MyApp());
+// @override
+// InitializationState createState() => InitializationState();
 
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
 
-  @override
-  _MyAppState createState() => _MyAppState();
 
-}
-
-String speaker = "nara";
+String speaker = "dain";
 String volume = "0";
 String emotion = "0";
 String speed = "0";
@@ -23,35 +19,53 @@ String format = "mp3";
 String text = "안녕 호빈 친구";
 
 
-String client_id = "";
-String client_secret = "";
-String lang = "Kor";
-String url = 'https://naveropenapi.apigw.ntruss.com/voice-premium/v1/tts' + lang;
+String client_id = "oipjydjt49";
+String client_secret = "RpmjMMy7caPJ3Hura6ALqRbxTDm5QaUcU1fPLVg8";
+String url = 'https://naveropenapi.apigw.ntruss.com/voice-premium/v1/tts';
 
-class _MyAppState extends State<MyApp> {
+
+class textToSpeech extends StatefulWidget {
+
+  @override
+  textToSpeechState createState() => textToSpeechState();
+
+}
+
+class textToSpeechState extends State<textToSpeech> {
+
+  void _createPost() async {
+    final response = await http.post(
+      url,
+      body: jsonEncode(
+        {
+          "speaker" : speaker,
+          "volume" : volume,
+          "emotion" : emotion,
+          "text" : text,
+          "speed" : speed,
+          "pitch" : pitch,
+          "format" : format
+        },
+      ),
+      headers: {"X-NCP-APIGW-API-KEY-ID": client_id ,"X-NCP-APIGW-API-KEY" : client_secret},
+    );
+    AudioCache player = new AudioCache();
+
+    //asset에 있는거 접근할 때, const AudioPath = "click1.mp3"
+
+    player.play(response.body);
+  }
+
   @override
   void initState() {
-    void _createPost() async {
-      final response = await http.post(
-        url,
-        body: jsonEncode(
-          {
-            "speaker" : speaker,
-            "volume" : volume,
-            "emotion" : emotion,
-            "text" : text,
-            "speed" : speed,
-            "pitch" : pitch,
-            "format" : format
-          },
-        ),
-        headers: {"X-NCP-APIGW-API-KEY-ID": "NULL","X-NCP-APIGW-API-KEY-ID" : "NULL"},
-      );
-    }
+    super.initState();
+    _createPost();
+
   }
 
   @override
   Widget build(BuildContext context) {
+
 
   }
 }
