bc4fa4dfc8ac512be866eae9a7f5358de2691d18
unknown <ghqls1237@kaist.ac.kr>
2021-02-09 13:29:10 +0900

    connect to NAVER clova API

--- paran_girin/lib/TTS/ttsTest.dart
+++ paran_girin/lib/TTS/ttsTest.dart
@@ -10,7 +10,7 @@ import 'package:audioplayers/audio_cache.dart';
 
 
 
-String speaker = "dain";
+String speaker = "ndain";
 String volume = "0";
 String emotion = "0";
 String speed = "0";
@@ -21,7 +21,7 @@ String text = "안녕 호빈 친구";
 
 String client_id = "oipjydjt49";
 String client_secret = "RpmjMMy7caPJ3Hura6ALqRbxTDm5QaUcU1fPLVg8";
-String url = 'https://naveropenapi.apigw.ntruss.com/voice-premium/v1/tts';
+String url = 'https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts';
 
 
 class textToSpeech extends StatefulWidget {
@@ -32,11 +32,10 @@ class textToSpeech extends StatefulWidget {
 }
 
 class textToSpeechState extends State<textToSpeech> {
-
   void _createPost() async {
     final response = await http.post(
       url,
-      body: jsonEncode(
+      body:
         {
           "speaker" : speaker,
           "volume" : volume,
@@ -46,14 +45,15 @@ class textToSpeechState extends State<textToSpeech> {
           "pitch" : pitch,
           "format" : format
         },
-      ),
-      headers: {"X-NCP-APIGW-API-KEY-ID": client_id ,"X-NCP-APIGW-API-KEY" : client_secret},
+      headers: {"X-NCP-APIGW-API-KEY-ID": client_id ,"X-NCP-APIGW-API-KEY" : client_secret, "Content-Type": "application/x-www-form-urlencoded"},
     );
-    AudioCache player = new AudioCache();
 
+    AudioCache player = new AudioCache();
+    print(response);
+    print("body : " + response.body);
     //asset에 있는거 접근할 때, const AudioPath = "click1.mp3"
 
-    player.play(response.body);
+   // player.play(response.body);
   }
 
   @override
@@ -65,7 +65,7 @@ class textToSpeechState extends State<textToSpeech> {
 
   @override
   Widget build(BuildContext context) {
-
+    return Text("성공했땅");
 
   }
 }
